9be49af2caf70dadda1e5aa1e73f4e61cf97c399
Seven <seven@51shepherd.com>
2013-03-28 16:21:44 +0800

    Finish sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by_email(params[:session][:email])
-  	if user && user.authenticate(params[:session][:password])
-  	  # Sign the user in and redirect to the user's show page.
-  	else
-  	  flash[:error] = 'Invalid email/password combination' #Not quite right!
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
+    sign_out
+    redirect_to root_path
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,18 +18,24 @@ describe "Authentication" do
 
       it { should have_selector('title', text: 'Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-      	fill_in "Email", with: user.email
-      	fill_in "Password", with: user.password
-      	click_button "Sign in"
+       fill_in "Email", with: user.email
+       fill_in "Password", with: user.password
+       click_button "Sign in"
       end
-      it { should have_selector('title', text: user.name)}
+
+      it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
-      it { should have_selector('Sign out', href: signout_path) }
+      it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
   end
