113b737d646f4a6619275fc5c1e0bef0e6c66649
seven <seven@seven-K40IE.(none)>
2013-05-02 18:28:35 +0800

    write code 9.11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,5 +21,17 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-  end 
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+      #Handle a successful update.
+    else
+      render 'edit'
+    end
+  end    
 end
